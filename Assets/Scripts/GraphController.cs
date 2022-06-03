4f1a099b623b6a131dfe52880af67af88f97d16a
dushes <alex1702.96pablo@yandex.ru>
2022-06-03 22:06:27 +0300

    Init

---
+++ Assets/Scripts/GraphController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DefaultNamespace
+{
+    public class GraphController
+    {
+        private Node[] _nodes;
+
+        public GraphController(ICollection nodeViews)
+        {
+            _nodes = new Node[nodeViews.Count];
+
+            for (int i = 0; i < nodeViews.Count; i++)
+            {
+                _nodes[i] = new Node();
+            }
+        }
+    }
+}
